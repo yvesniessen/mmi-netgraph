b60fa34da5b8085a76bdaa5d2ee31a61831d6a62
sch.thomas@gmail.com <sch.thomas@gmail.com@f4f2bab1-4c82-1f48-62a8-25d0d5d5c5f6>
2012-04-02 14:17:18 +0000

    Strategy-Pattern implementiert

---
+++ trunk/NETGraph/NETGraph/GraphAlgorithms/DepthSearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NETGraph.Algorithm
+{
+    class DepthSearch : IGraphAlgorithm
+    {
+
+        #region IGraphAlgorithm Member
+
+        public Graph performAlgorithm(Graph graph, Vertex<string> startVertex)
+        {
+            Graph result = new Graph();
+
+            Stack<Vertex<String>> stack = new Stack<Vertex<string>>();
+
+            if (!startVertex.Marked)
+            {
+                startVertex.Marked = true;
+                result.Vertexes.Add(startVertex);
+
+                foreach (Vertex<String> v in startVertex.findNeighbors(graph.DirectedEdges))
+                {
+                    if (!v.Marked && !stack.Contains(v))
+                    {
+                        stack.Push(v);
+                    }
+                }
+
+                while (stack.Count != 0)
+                {
+                    Vertex<String> currentvertex = stack.Pop();
+
+                    Graph tmp2 = performAlgorithm(result, currentvertex);
+
+                    foreach (Vertex<String> s in tmp2.Vertexes)
+                    {
+                        result.Vertexes.Add(s);
+                    }
+                }
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/NETGraph/NETGraph/GraphAlgorithms/IGraphAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NETGraph.Algorithm
+{
+    interface IGraphAlgorithm
+    {
+        Graph performAlgorithm(Graph graph, Vertex<String> startVertex);
+    }
+}
