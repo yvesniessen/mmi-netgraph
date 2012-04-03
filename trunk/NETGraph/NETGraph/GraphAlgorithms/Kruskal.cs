f1457d58a29b8d9472d439c2f3babeb907a87bc9
sch.thomas@gmail.com <sch.thomas@gmail.com@f4f2bab1-4c82-1f48-62a8-25d0d5d5c5f6>
2012-04-03 17:42:43 +0000

    initial commit

---
+++ trunk/NETGraph/NETGraph/GraphAlgorithms/Kruskal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NETGraph.Algorithm;
+
+namespace NETGraph.GraphAlgorithms
+{
+    class Kruskal : IGraphAlgorithm
+    {
+        public Graph performAlgorithm(Graph graph, Vertex<string> startVertex)
+        {
+            Graph T = new Graph();
+            Graph temp = new Graph();
+
+            temp.Vertexes = graph.Vertexes;
+
+            foreach (Vertex<String> v in temp.Vertexes)
+            {
+                foreach (Edge e in v.Edges)
+                {
+                    temp.deleteEdge(e);
+                }
+            }
+
+            T.Vertexes = temp.Vertexes;
+
+            List<Edge> edges = graph.Edges;
+
+            edges.Sort(delegate(Edge e1, Edge e2) { return e1.Costs.CompareTo(e2.Costs); });
+
+            while(edges.Count > 0)
+            {
+                //Hole die günstigste Kante aus der Liste und entferne sie
+                Edge currentEdge = edges.First();
+                edges.Remove(currentEdge);
+
+                temp.addEdge(currentEdge.StartVertex, currentEdge.EndVertex, currentEdge.Costs);
+
+                if (temp.getConnectingComponents().Count < T.getConnectingComponents().Count)
+                {
+                    T.addEdge(currentEdge.StartVertex, currentEdge.EndVertex, currentEdge.Costs);
+                }
+                else
+                {
+                    temp.deleteEdge(currentEdge);
+                }
+
+                temp.addEdge(currentEdge.StartVertex, currentEdge.EndVertex, currentEdge.Costs);
+            }
+
+            return T;
+        }
+
+    }
+}
